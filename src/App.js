996eec19038d90aff0ed0d43bcf95e374e9628ba
PuchNik <i.p.chn.k@gmail.com>
2024-10-31 15:21:11 +0300

    Описал декларативные и императивные строки кода (в Первом коммите забыл это сделать)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  const nowYear = new Date().getFullYear()
+  const nowYear = new Date().getFullYear() // Императивный
 
+      // Все остальное Декларативный
   return (
     <div className="App">
       <header className="App-header">
