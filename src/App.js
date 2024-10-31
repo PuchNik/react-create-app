0eed40f2914408ba8ca369e889a74dd6bc76938a
PuchNik <i.p.chn.k@gmail.com>
2024-10-31 15:24:33 +0300

    Переписал .jsx на .js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  const nowYear = new Date().getFullYear() // Императивный
+  const currentYear = new Date().getFullYear();
 
-      // Все остальное Декларативный
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{nowYear}</p>
-      </header>
-    </div>
+  return React.createElement(
+      'div',
+      { className: 'App' },
+      React.createElement(
+          'header',
+          { className: 'App-header' },
+          React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+          React.createElement('p', null, 'Edit ', React.createElement('code', null, 'src/App.js'), ' and save to reload.'),
+          React.createElement(
+              'a',
+              {
+                className: 'App-link',
+                href: 'https://reactjs.org',
+                target: '_blank',
+                rel: 'noopener noreferrer',
+              },
+              'Learn React'
+          ),
+          React.createElement('p', null, currentYear)
+      )
   );
 }
-
